018ec2e73ae7c1f23a6a487f5c7ce45607b9d48e
snowkrebs <snow.krebs@gmail.com>
2014-08-15 17:28:01 -0700

    upload new link

--- testpart/includes/alphabetWidget.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$alphabetStringHtml = "<div id=\"alphbetWrapper\">";
-		$alphabetStringHtml .= '<a href="#">#</a>';
-		foreach(range('a','z') as $i) {
-			$alphabetStringHtml .= '<a href="/artist.php?letter=' . strtoupper($i) . '">' . strtoupper($i) . '</a>';
-		}
-	$alphabetStringHtml .= '</div>';
-	echo $alphabetStringHtml;
-?>
